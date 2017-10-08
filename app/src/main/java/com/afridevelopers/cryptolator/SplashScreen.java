195ca34201852e3cf076f2e4c0aa43a9a807dc39
Rising Odegua <williamsrising592@gmail.com>
2017-10-08 21:19:43 +0100

    Added the MainActivity.

--- app/src/main/java/com/afridevelopers/cryptolator/SplashScreen.java
+++ app/src/main/java/com/afridevelopers/cryptolator/SplashScreen.java
@@ -1,13 +1,45 @@
 package com.afridevelopers.cryptolator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private ImageView splashImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        splashImage = (ImageView)findViewById(R.id.splashImage);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.transition);
+        splashImage.startAnimation(animation);
+
+        final Intent intent = new Intent(this, MainActivity.class);
+
+        Thread timer = new Thread(){
+            @Override
+            public void run() {
+                try {
+
+                    sleep(4000);
+
+                }catch (Exception e){
+                   e.printStackTrace();
+                }finally {
+
+                    startActivity(intent);
+                    SplashScreen.this.finish();
+                }
+
+            }
+        };
+        timer.start();
+
     }
 }
