7d18d6e22048a7ffd1d51c56ab83b7215c38ad23
Rising Odegua <williamsrising592@gmail.com>
2017-10-10 22:48:13 +0100

    Added UI design for about activity

--- app/src/main/java/com/afridevelopers/cryptolator/About.java
+++ app/src/main/java/com/afridevelopers/cryptolator/About.java
@@ -2,12 +2,14 @@ package com.afridevelopers.cryptolator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class About extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_about);
     }
 }
