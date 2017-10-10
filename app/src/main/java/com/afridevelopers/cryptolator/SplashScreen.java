242caf8091fe91d8d0409b3ed500689f9df7e2d4
Rising Odegua <williamsrising592@gmail.com>
2017-10-10 08:24:03 +0100

    Removed the live trading activity and added the httphandler class

--- app/src/main/java/com/afridevelopers/cryptolator/SplashScreen.java
+++ app/src/main/java/com/afridevelopers/cryptolator/SplashScreen.java
@@ -1,19 +1,22 @@
 package com.afridevelopers.cryptolator;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity {
 
     private ImageView splashImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash_screen);
 
         splashImage = (ImageView)findViewById(R.id.splashImage);
